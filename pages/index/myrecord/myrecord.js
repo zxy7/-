66e48d5c7910efa5135a161cb25bafacbf184209
昝晓玉 <xyzan@jinhetech.com>
2018-01-17 17:19:11 +0800

    save

--- pages/index/myrecord/myrecord.js
+++ pages/index/myrecord/myrecord.js
@@ -8,6 +8,43 @@ Page({
   data: {
     mineRecod: 1,
     userInfo: [],
+    money:1.2,
+    num:1,
+    sendrecords:[
+      {
+        recordid:'1',
+        title: "aaaaa",
+        allmoney:20,
+        time:111111,
+        restnum:1,
+        allnum:1
+      },
+      {
+        recordid: '2',
+        title: "aaaaa",
+        allmoney:20,
+        time:111111,
+        restnum:1,
+        allnum:1
+      },
+    ],
+    getrecords: [
+      {
+        recordid: '1',
+        name: "aaaaa",
+        imgsrc: "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKCPhTeW9YgEAA5JMicOQBobibXx7O4V0G7S7TXic0lyzDL5LrPCQKIOCsSDTH0uY6np4kZz7kScPusQ/0",
+        getmoney: 1.0,
+        time: 111111
+      },
+      {
+        recordid: '2',
+        name: "aaaaa",
+        imgsrc: "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKCPhTeW9YgEAA5JMicOQBobibXx7O4V0G7S7TXic0lyzDL5LrPCQKIOCsSDTH0uY6np4kZz7kScPusQ/0",
+        getmoney: 2.0,
+        time: 111111
+      },
+
+    ],
   },
 
   /**
